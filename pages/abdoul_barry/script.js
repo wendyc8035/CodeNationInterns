7014f5be24cb582ac18271f3fe823f01294d2d3c
Peter Jablonski <pwjablonski@gmail.com>
2018-07-23 12:18:22 -0400

    Revert "Revert "I joined the project!""

---
+++ pages/abdoul_barry/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+    var searchTerm;
+   
+    $('#search').click(function(){
+        search();
+    });
+
+    $('input').keypress(function(e){
+        if(e.key === "Enter") {
+            search();
+        }
+    });
+
+    function search(){
+        var searchTerm = $("input").val();
+        searchTerm = searchTerm.toLowerCase();
+        if (searchTerm === ''){
+            $(".intern-container").show();
+        } else {
+            var interContainers = $(".intern-list")
+
+            var searchResult = interContainers.find(`.${searchTerm}`);
+    
+            $(".intern-container").hide();
+            
+            searchResult.show()
+        }
+    }
+
+
+});
