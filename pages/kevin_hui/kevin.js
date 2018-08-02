aeaf9646a4d54f786ce37b9abe316f21d778d5c5
kevinhui98 <kevin.hui@envirostudies.org>
2018-08-02 14:47:04 +0000

    adding the song

--- pages/kevin_hui/kevin.js
+++ pages/kevin_hui/kevin.js
@@ -1,16 +1,16 @@
 //soundcloud api
-// SC.initialize({
-//   client_id: '92929233352ef9e50c2c4e051b0aadf3'
-// });
+SC.initialize({
+  client_id: '92929233352ef9e50c2c4e051b0aadf3'
+});
 
-// // streaming blackpink forever young
-// SC.stream('/tracks/458705343').then(function(player){
-//   player.play().then(function(){
-//     console.log('Playback started!');
-//   }).catch(function(e){
-//     console.error('Playback rejected. Try calling play() from a user interaction.', e);
-//   });
-// });
+// streaming blackpink forever young
+SC.stream('/tracks/458705343').then(function(player){
+  player.play().then(function(){
+    console.log('Playback started!');
+  }).catch(function(e){
+    console.error('Playback rejected. Try calling play() from a user interaction.', e);
+  });
+});
 
 $(document).ready(function(){
   $("#proPic").click(function(){
@@ -25,16 +25,4 @@ $(document).ready(function(){
     $("#proPic").attr( "src" , "stockPic.jpg" );
     console.log("bye bye");
   });
-  
-//("#proPic").onmouseover = function() {mouseOver()};
-// document.getElementById("proPic").onmouseout = function() {mouseOut()};
-// console.log("hi");
-
-// function mouseOver(){
-// document.getElementById("proPic").src = "kevin.jpg";
-//     //<img class="rounded-circle profile-img" src="kevin.jpg" alt="">
-// }
-// function mouseOut(){
-//     document.getElementById('proPic').innerHTML = "src ='stockPic.jpg'";
-// }
 });
