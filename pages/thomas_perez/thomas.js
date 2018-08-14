4248b07b195eeb160603f0583cb9c3cdc86b1db5
Thomas Perez <thomasperez401@gmail.com>
2018-08-14 20:06:54 +0000

    remove hover function on header

--- pages/thomas_perez/thomas.js
+++ pages/thomas_perez/thomas.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
         e.preventDefault();
         window.open("mailto:thomasperez401@gmail.com");
     });
-    $("#profile").hover(function(){
-        $(this).attr("src", "young_me.JPG"); 
-    }, function(){
-        $(this).attr("src", "headshot.jpg");
-    });
+    // $("#profile").hover(function(){
+    //     $(this).attr("src", "young_me.JPG"); 
+    // }, function(){
+    //     $(this).attr("src", "headshot.jpg");
+    // });
 });
